a345346577b13709dd1de71e5fe2b982e2ac350b
Marc Nuri <marc@marcnuri.com>
2022-03-29 21:06:23 +0200

    feat: Spring Boot 2.6.5 with Maven

---
+++ spring-maven/src/main/java/com/example/demo/Resource.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class Resource {
+
+  @GetMapping
+  public String hello() {
+    return "Hello";
+  }
+}
